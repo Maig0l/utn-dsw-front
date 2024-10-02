a039b55aaefc4e01c75fb5ea0792b3451a0e362d
LuchoCsd <lucianocasado99@gmail.com>
2024-10-01 23:38:25 -0300

    basic login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,12 @@ import { StudioComponent } from './components/studio/studio.component.js';
 import { HeaderComponent } from './shared/header/header.component.js';
 import { FooterComponent } from './shared/footer/footer.component.js';
 import { LoginComponent } from './auth/login/login.component.js';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LoginComponent, RouterOutlet, NgFor, PlatformComponent,ShopComponent, StudioComponent, HeaderComponent, FooterComponent],
+  imports: [ReactiveFormsModule, LoginComponent, RouterOutlet, NgFor, PlatformComponent,ShopComponent, StudioComponent, HeaderComponent, FooterComponent],
   // Registramos los servicios que nos traen los datos del backend
   //providers: [ShopService, PlatformService],
   templateUrl: './app.component.html',
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder,Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm = this.fb.group({
+    username: ['', Validators.required], //, Validators.minLength(6) lo rompe todo? 
+    password: ['', Validators.required]
+  });
+  constructor(private fb: FormBuilder, private router: Router) {}
 
+  login() {
+    if(this.loginForm.valid) {  
+    console.log("Call login service");
+    this.router.navigateByUrl('/platform'); //TODO
+    this.loginForm.reset();
+  } else {
+    alert("Form is invalid");
+  } 
+  }
 }

d25444e58d0e769a2b453e338fd4434a151a724e
RST5150 <rstvh5150@gmail.com>
2024-11-02 21:14:13 -0300

    feat: Review added

---
+++ src/app/components/services/review.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+
+export interface Review {
+  id: number
+  author_id: number
+  created_at: Date
+  game_id: number
+  score: number /*for now*/
+  title: string
+  body: string
+}
+
+interface ApiResponse {
+  message: string
+  data: any
+}
+
+@Injectable()
+export class ReviewService {
+  constructor(private http: HttpClient) { }
+
+  // TODO: Guardar la URL base y rutas de endpoints en algún archivo de config global
+  reviewsEndpoint = "http://localhost:8080/api/reviews"
+
+  getAllReviews(): Observable<Review[]> {
+    return this.http.get<ApiResponse>(this.reviewsEndpoint)
+      // Devuelve lo que está dentro de data en el objeto de respuesta
+      .pipe(map(response => response.data))
+    }
+
+  addReview(author_id: number, created_at: string, game_id: number, score: number, title: string, body: string): Observable<Review> {
+    return this.http.post<Review>(this.reviewsEndpoint, { author_id, created_at, game_id,score,title, body })
+  }
+
+ 
+}

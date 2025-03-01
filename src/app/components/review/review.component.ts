275fb80dacce18435270622fe01d3b6669cb1209
Miguel Cabanellas <miguel.cabanellas03@gmail.com>
2025-02-28 21:18:51 -0300

    fix: broken import?

--- src/app/components/review/review.component.ts
+++ src/app/components/review/review.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Game, GameService } from '../services/game.service.js';
 import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
-import { Review, ReviewService } from '../services/review.service.js';
+import { IReview, ReviewService } from '../services/review.service.js';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
@@ -21,27 +21,27 @@ export class ReviewComponent {
 
   reviewForm = new FormGroup({
 
-   
+
     score: new FormControl(0),
     title: new FormControl(''),
     body: new FormControl(''),
-    
+
 
   });
 
   gameId!: number;
   game!: Game;
-  review!: Review;
-  
+  review!: IReview;
+
   constructor( private route: ActivatedRoute, private gameService: GameService, private  reviewService: ReviewService, private router: Router ) {}
-    
+
   ngOnInit() {
       // Obtén el ID del juego desde la URL
       this.gameId = +this.route.snapshot.paramMap.get('id')!
       // Usa el servicio para obtener los detalles del juego
       this.getGameDetails();
   }
-  
+
   getGameDetails() {
       this.gameService.getOneGame(this.gameId).subscribe(response  => {
           this.game = response;
@@ -50,7 +50,7 @@ export class ReviewComponent {
 
   addReview(){
     const score = Number.parseInt(this.reviewForm.value.score?.toString() ?? '0');
-    
+
     this.reviewService.addReview(
       1, /*falta implementar usuario*/
       this.gameId,

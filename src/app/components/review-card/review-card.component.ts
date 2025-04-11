08b96dbca706a86f34ba304d166fdad99c8d530f
RST5150 <rstvh5150@gmail.com>
2025-04-11 10:20:14 -0300

    Added star rating (moon) to user, review. review-card

--- src/app/components/review-card/review-card.component.ts
+++ src/app/components/review-card/review-card.component.ts
@@ -1,13 +1,17 @@
 import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Review } from '../../model/review.model';
+import { environment } from '../../../enviroment/enviroment.js';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-review-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './review-card.component.html',
   styleUrl: './review-card.component.css',
 })
 export class ReviewCardComponent {
   review = input.required<Review>();
+  apiUrl = environment.apiUrl;
 }

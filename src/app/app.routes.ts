423b294b209ec4b13e532f508d80fdbf536c1315
Miguel Cabanellas <miguel.cabanellas03@gmail.com>
2025-07-05 11:49:30 -0300

    refactor: move user component to sites directory for structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,7 +16,7 @@ import { GameDetailsComponent } from './pages/game-details/game-details.componen
 import { EditGameComponent } from './components/edit-game/edit-game.component';
 import { ViewTagComponent } from './components/view-tag/view-tag.component';
 import { SearchFiltersComponent } from './pages/search-filters/search-filters.component';
-import { UserComponent } from './components/user/user.component';
+import { UserComponent } from './pages/user/user.component';
 import { UserEditComponent } from './components/user-edit/user-edit.component';
 import { PlaylistEditComponent } from './components/playlist-edit/playlist-edit.component';
 

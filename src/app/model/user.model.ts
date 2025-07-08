6ab666cb3b1a8f3f3123c2acafad786d7fef97b3
Miguel Cabanellas <miguel.cabanellas03@gmail.com>
2025-07-08 11:54:38 -0300

    Add tag chips to User Profile

--- src/app/model/user.model.ts
+++ src/app/model/user.model.ts
@@ -2,14 +2,13 @@ import { Playlist } from './playlist.model';
 import { Review } from './review.model.js';
 import { Tag } from './tag.model.js';
 
-
 export interface User {
   id: number;
   nick: string;
   email: string;
   profile_img?: string;
   bio_text?: string;
-  linked_accounts?: string[];
+  linked_accounts: string[];
   playlists: Playlist[];
   likedTags: Tag[];
   reviews: Review[];

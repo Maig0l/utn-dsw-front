85a00820ade6682164f4f824ef48a1d2853690bf
LuchoCsd <lucianocasado99@gmail.com>
2025-07-10 19:31:16 -0300

    Visual improvements on crud component

--- src/app/pages/cruds/cruds.component.ts
+++ src/app/pages/cruds/cruds.component.ts
@@ -1,25 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatSidenavModule} from '@angular/material/sidenav';
-import { MatListModule} from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-
-
-
-
-
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-cruds',
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, MatListModule, RouterModule, MatToolbarModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './cruds.component.html',
-  styleUrl: './cruds.component.css'
+  styleUrl: './cruds.component.css',
 })
-export class CrudsComponent {
-
-}
+export class CrudsComponent {}

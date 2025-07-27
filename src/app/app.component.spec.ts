a7d035956a268e65ce2ad37fa2b07980ae656a23
Matias <matiasanin15@gmail.com>
2025-07-27 13:29:54 -0300

    fix app test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, HttpClientModule],
+      imports: [AppComponent, HttpClientModule], providers: [JwtHelperService, { provide: JWT_OPTIONS, useValue: JWT_OPTIONS }]
     }).compileComponents();
   });
 

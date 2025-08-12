3ac6f2e7475a46c346f0ede2748580709902173a
Miguel Cabanellas <miguel.cabanellas03@gmail.com>
2025-08-12 17:05:42 -0300

    fix: use correct api endpoint on dev env

--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -5,5 +5,5 @@ const API_PORT = 8080;
 
 export const environment = {
   production: false,
-  apiUrl: import.meta.env.NG_APP_API_URL || `http://${API_HOST}:${API_PORT}`,
+  apiUrl: import.meta.env.NG_APP_API_URL || `http://${API_HOST}:${API_PORT}/api`,
 };
